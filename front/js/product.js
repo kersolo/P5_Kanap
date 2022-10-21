adee77fe9853d547335978f7bd80eeb8284b92f9
kersolo <loick.ferelloc@gmail.com>
2022-10-21 11:14:11 +0200

    refctorisation pages acceuil et products

--- front/js/product.js
+++ front/js/product.js
@@ -1,40 +1,152 @@
-const getId = new URL(document.location.href).searchParams.get('id');
+const getItemId = () => {
+  return new URL(location.href).searchParams.get('id');
+};
 
-let url = 'http://localhost:3000/api/products/' + getId;
-///////////////////////////////
-let tab = fetch(url).then((res) => res.json());
-console.log(tab);
-tab.then((productData) => console.log(productData));
+const mainFunction = async () => {
+  const itemId = getItemId();
+  const item = await getItem(itemId);
+  showItems(item);
+};
 
-tab.then((productData) => {
-  let item = document.querySelector('.item__img');
+const getItem = async (itemId) => {
+  // const response = await fetch(`http://localhost:3000/api/products/${itemId}`);
+  const response = await fetch('http://localhost:3000/api/products/' + itemId);
+  const products = await response.json();
+  return products;
+};
+
+const showItems = (item) => {
+  let item2 = document.querySelector('.item__img');
   let title = document.querySelector('#title');
+
   let price = document.querySelector('#price');
   let img = document.createElement('img');
   let p = document.querySelector('#description');
   let colorsId = document.querySelector('#colors');
 
-  item.appendChild(img);
-  title.innerHTML = productData.name;
-  price.innerHTML = productData.price;
-  img.src = productData.imageUrl;
-  p.innerHTML = productData.description;
+  item2.appendChild(img);
+  title.innerHTML = item.name;
+  price.innerHTML = item.price;
+  img.src = item.imageUrl;
+  p.innerHTML = item.description;
 
-  for (const color of productData.colors) {
+  for (const color of item.colors) {
     let option = document.createElement('option');
-    console.log(option);
+    // console.log(option);
     option.innerHTML = color;
     colorsId.appendChild(option);
   }
-});
+};
 
-let firstTest = document.querySelector('.item__content__addButton');
-let testt = document.createElement('a');
-let button = document.querySelector('button');
-firstTest.appendChild(testt);
-testt.appendChild(button);
-console.log(testt);
-testt.href = './cart.html';
+mainFunction();
+
+/**************************/
+
+// const addProductInCart = async () => {
+//   const itemId = getItemId();
+//   const item = await getItem(itemId);
+//   showItems(item);
+// };
+
+// const getItemId = () => {
+//   return new URL(location.href).searchParams.get('id');
+// };
+// const getItem = (itemId) => {
+//   return fetch(`http://localhost:3000/api/products/${itemId}`)
+//     .then(function (httpBodyResponse) {
+//       return httpBodyResponse.json();
+//     })
+//     .then(function (product) {
+//       return product;
+//     })
+//     .catch(function (error) {
+//       alert('Erreur de chargement des produits');
+//     });
+// };
+
+// const showItems = (item) => {
+//   let item2 = document.querySelector('.item__img');
+//   let title = document.querySelector('#title');
+
+//   let price = document.querySelector('#price');
+//   let img = document.createElement('img');
+//   let p = document.querySelector('#description');
+//   let colorsId = document.querySelector('#colors');
+
+//   item2.appendChild(img);
+//   title.innerHTML = item.name;
+//   price.innerHTML = item.price;
+//   img.src = item.imageUrl;
+//   p.innerHTML = item.description;
+
+//   for (const color of item.colors) {
+//     let option = document.createElement('option');
+//     // console.log(option);
+//     option.innerHTML = color;
+//     colorsId.appendChild(option);
+//   }
+// };
+
+// addProductInCart();
+
+/****************************************/
+/****************************************/
+
+// const getId = new URL(document.location.href).searchParams.get('id');
+
+// console.log(getId);
+
+// let url = 'http://localhost:3000/api/products/' + getId;
+// ///////////////////////////////
+// let tab = fetch(url).then((res) => res.json());
+// // console.log(tab);
+// tab.then((productData) => console.log(productData));
+
+// tab.then((productData) => {
+//   let item = document.querySelector('.item__img');
+//   let title = document.querySelector('#title');
+//   let price = document.querySelector('#price');
+//   let img = document.createElement('img');
+//   let p = document.querySelector('#description');
+//   let colorsId = document.querySelector('#colors');
+
+//   item.appendChild(img);
+//   title.innerHTML = productData.name;
+//   price.innerHTML = productData.price;
+//   img.src = productData.imageUrl;
+//   p.innerHTML = productData.description;
+
+//   for (const color of productData.colors) {
+//     let option = document.createElement('option');
+//     // console.log(option);
+//     option.innerHTML = color;
+//     colorsId.appendChild(option);
+//   }
+// });
+
+// /****************************************/
+// /****************************************/
+
+// const btnDiv = document.querySelector('.item__content__addButton');
+// const btnA = document.createElement('a');
+// const btn = document.querySelector('button');
+// btnDiv.appendChild(btnA);
+// btnA.appendChild(btn);
+
+// btn.addEventListener('click', () => {
+//   btnA.href = './cart.html';
+// });
+
+/****************************************/
+/****************************************/
+
+// let firstTest = document.querySelector('.item__content__addButton');
+// let testt = document.createElement('a');
+// let button = document.querySelector('button');
+// firstTest.appendChild(testt);
+// testt.appendChild(button);
+// console.log(testt);
+// testt.href = './cart.html';
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////:
@@ -49,6 +161,4 @@ testt.href = './cart.html';
 // }
 
 ///////////////////////////////
-///////////////////////////////
-
 ///////////////////////////////
--- front/js/script.js
+++ front/js/script.js
@@ -1,38 +1,85 @@
-let url = 'http://localhost:3000/api/products';
-
-let tab = fetch(url).then((res) => res.json());
-// tab.then((data) => console.log(data));
-
-tab.then((productsData) => {
-  // for (let i = 0; i < data.length; i++) {
+const mainFunction = async () => {
+  const productsData = await fetchCall();
   for (const product of productsData) {
-    let items = document.querySelector('.items');
-    let a = document.createElement('a');
-    a.href = './product.html?id=' + product._id;
-    let article = document.createElement('article');
-    let img = document.createElement('img');
-    let h3 = document.createElement('h3');
-    let p = document.createElement('p');
-    h3.classList.add('productName');
-    p.classList.add('productDescription');
-
-    function append() {
-      items.appendChild(a);
-      a.appendChild(article);
-      article.appendChild(h3);
-      article.appendChild(img);
-      article.appendChild(p);
-    }
-    append();
-
-    function pourTest() {
-      img.src = product.imageUrl;
-      h3.innerText = product.name;
-      p.innerText = product.description;
-    }
-    pourTest();
+    createCards(product);
   }
-});
+};
+
+const fetchCall = async () => {
+  const response = await fetch('http://localhost:3000/api/products');
+  const products = await response.json();
+
+  return products;
+};
+
+const createCards = (product) => {
+  let items = document.querySelector('.items');
+  let a = document.createElement('a');
+  let article = document.createElement('article');
+  let img = document.createElement('img');
+  let h3 = document.createElement('h3');
+  let p = document.createElement('p');
+  h3.classList.add('productName');
+  p.classList.add('productDescription');
+
+  a.href = './product.html?id=' + product._id;
+
+  items.appendChild(a);
+  a.appendChild(article);
+  article.appendChild(h3);
+  article.appendChild(img);
+  article.appendChild(p);
+
+  img.src = product.imageUrl;
+  h3.innerText = product.name;
+  p.innerText = product.description;
+};
+
+mainFunction();
+
+/****************************************/
+/****************************************/
+/****************************************/
+
+// let url = 'http://localhost:3000/api/products';
+
+// let tab = fetch(url).then((res) => res.json());
+// // tab.then((data) => console.log(data));
+
+// tab.then((productsData) => {
+//   // for (let i = 0; i < data.length; i++) {
+//   for (const product of productsData) {
+//     let items = document.querySelector('.items');
+//     let a = document.createElement('a');
+//     a.href = './product.html?id=' + product._id;
+//     let article = document.createElement('article');
+//     let img = document.createElement('img');
+//     let h3 = document.createElement('h3');
+//     let p = document.createElement('p');
+//     h3.classList.add('productName');
+//     p.classList.add('productDescription');
+
+//     function append() {
+//       items.appendChild(a);
+//       a.appendChild(article);
+//       article.appendChild(h3);
+//       article.appendChild(img);
+//       article.appendChild(p);
+//     }
+//     append();
+
+//     function pourTest() {
+//       img.src = product.imageUrl;
+//       h3.innerText = product.name;
+//       p.innerText = product.description;
+//     }
+//     pourTest();
+//   }
+// });
+
+/****************************************/
+/****************************************/
+/****************************************/
 
 // ///////////////////////////////
 // let url = 'http://localhost:3000/api/products';
